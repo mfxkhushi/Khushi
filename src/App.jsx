b39f940c5992c8830341a57a8dcea65a8ade0d4b
amitverma8835 <amitkv93047@gmail.com>
2026-03-18 17:26:25 +0530

    Fix blank screen issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import First from './pages/FirstPage/First'
 import Second from './pages/SecondPage/Second';
 import OverView from './pages/OverView/OverView';
