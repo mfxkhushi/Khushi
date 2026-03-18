2792df16e40f764e944dba6ec61ae68c0f3f5ec0
amitverma8835 <amitkv93047@gmail.com>
2026-03-18 17:34:07 +0530

    Critical fix for GitHub Pages blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,19 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<First />} /> */}
         <Route path="/" element={<Second />} />
         <Route path="/overview" element={<OverView/>} />
         <Route path="/location" element={<Location/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/gallery" element={<Gallery/>} />
-
-        
         <Route path="/specs" element={<Specs/>} />
-        {/* <Route path="/outlets" element={<Outlets/>} /> */}
-        {/* <Route path="/page/:floor" element={<Page />} /> */}
         <Route path="/amenities" element={<Amenities />} />
         <Route path="/plans" element={<Plans />} />
-         <Route path="/why-green-building" element={<WhyGreen />} />
+        <Route path="/why-green-building" element={<WhyGreen />} />
         <Route path="/floor-plan/:planType" element={<FloorPage />}/>
-
+        {/* Redirect any unknown route to home */}
+        <Route path="*" element={<Second />} />
       </Routes>
     </Router>
   );
